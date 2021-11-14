295701404a05939f456573da94a682e550616f98
gwvsol <gwvsol@gmail.com>
2021-11-14 22:17:34 +0300

    добавлены основные файлы проекта приложения

---
+++ webscan/webscan/__init__.py
@@ -0,0 +1,21 @@
+"""
+Сервис с сканирования QR кодов с помощью web камеры
+"""
+
+__author__ = 'Fedorov Mikhail'
+__maintainer__ = 'Fedorov Mikhail'
+
+__email__ = 'jwvsol@yandex.ru'
+__license__ = 'MIT'
+__version__ = '0.4.0'
+__doc__ = 'Service with scanning QR codes using a web camera'
+
+
+__all__ = (
+    __author__,
+    __email__,
+    __license__,
+    __maintainer__,
+    __version__,
+    __doc__,
+)
