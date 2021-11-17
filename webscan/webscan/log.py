0069a970ef857586b217c419917f2b8f895a11a5
gwvsol <gwvsol@gmail.com>
2021-11-17 22:52:44 +0300

    изменение библиотеки логирования

--- webscan/webscan/log.py
+++ webscan/webscan/log.py
@@ -1,7 +1,11 @@
-import logging
+# import logging
+from loguru import logger as logging
 
-log_format = '%(asctime)s.%(msecs)d|\
-%(levelname)s|%(module)s.%(funcName)s:%(lineno)d %(message)s'
-logging.basicConfig(level=logging.INFO,
-                    format=log_format,
-                    datefmt='%Y-%m-%d %H:%M:%S')
+# log_format = '%(asctime)s.%(msecs)d|\
+# %(levelname)s|%(module)s.%(funcName)s:%(lineno)d %(message)s'
+# logging.basicConfig(level=logging.INFO,
+#                     format=log_format,
+#                     datefmt='%Y-%m-%d %H:%M:%S')
+
+logging.add(str, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green>|\
+{level}|<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> {message}")
